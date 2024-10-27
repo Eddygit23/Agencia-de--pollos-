c228b2334ef04c6a38a583274222bbcb59bbb219
Eddygit23 <eddycallisaya43@gmail.com>
2024-10-26 21:54:17 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+﻿// Función para simular añadir producto al carrito
+function mostrarAlerta() {
+    alert("Producto añadido al carrito, para que inserte su tarjeta de crédito");
+}
+
+// Validación de formularios
+document.getElementById("formContacto").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Formulario de contacto enviado. ¡Gracias por tu mensaje!");
+});
+
+document.getElementById("formSuscripcion").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("¡Gracias por suscribirte a nuestras ofertas!");
+});
+
+
+
+// Función boton de producto 1
+function mostrarAlerta1() {
+    alert("ver más información del producto");
+    window.location.href="indexProducto1.html";
+}
+
+// Función boton de producto 2
+function mostrarAlerta2() {
+    alert("ver más información del producto");
+    window.location.href="indexProducto2.html";
+}
+
+// Función boton de producto 3
+function mostrarAlerta3() {
+    alert("ver más información del producto");
+    window.location.href="indexProducto3.html";
+}
+
+// Función boton de producto 4
+function mostrarAlerta4() {
+    alert("ver más información del producto");
+    window.location.href="indexProducto4.html";
+}
+
+// Función boton de producto 5
+function mostrarAlerta5() {
+    alert("ver más información del producto");
+    window.location.href="indexProducto5.html";
+}
+
+// Función boton de producto 6
+function mostrarAlerta6() {
+    alert("ver más información del producto");
+    window.location.href="indexProducto6.html";
+}
+
+// Función boton de producto 7
+function mostrarAlerta7() {
+    alert("ver más información del producto");
+    window.location.href="indexProducto7.html";
+}
+
+// Función boton de producto 8
+function mostrarAlerta8() {
+    alert("ver más información del producto");
+    window.location.href="indexProducto8.html";
+}
+
+// Función boton de producto 9
+function mostrarAlerta9() {
+    alert("ver más información del producto");
+    window.location.href="indexProducto9.html";
+}
